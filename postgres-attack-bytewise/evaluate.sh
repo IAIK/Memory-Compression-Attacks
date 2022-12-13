9a1ca65939b5e9894b1c0ac94dc9ba0800206ce1
mschwarzl <m.schwarzl@student.tugraz.at>
2022-12-13 14:04:14 +0100

    Update evaluate.sh

--- postgres-attack-bytewise/evaluate.sh
+++ postgres-attack-bytewise/evaluate.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p results
 for i in {1..20}
 do
-    ./dict_attack.py ./TEMPLATE -h 147.75.100.61 -i enp7s0 -o results/test_$i.png
+    ./dict_attack.py ./TEMPLATE -h 127.0.0.1 -i enp7s0 -o results/test_$i.png
     killall dumpcap || true
     mkdir -p results/$i
     mv measurements*.csv results/$i/
